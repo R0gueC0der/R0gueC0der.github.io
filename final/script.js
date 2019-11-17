9002d52c595115653a7bb61670a4d3deca6561a7
R0gueC0der <52226836+R0gueC0der@users.noreply.github.com>
2019-11-17 22:38:06 +0530

    Add files via upload

--- final/script.js
+++ final/script.js
@@ -1,75 +1,76 @@
 class TypeWriter {
   constructor(txtElement, words, wait = 3000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = '';
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
-    this.type();
-    this.isDeleting = false;
+    this.txtElement = txtElement
+    this.words = words
+    this.txt = ''
+    this.wordIndex = 0
+    this.wait = parseInt(wait, 10)
+    this.type()
+    this.isDeleting = false
   }
 
   type() {
-    const current = this.wordIndex % this.words.length;
-    const fullTxt = this.words[current];
+    const current = this.wordIndex % this.words.length
+    const fullTxt = this.words[current]
 
-    if(this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1)
     } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
+      this.txt = fullTxt.substring(0, this.txt.length + 1)
     }
 
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
 
-    let typeSpeed = 100;
+    let typeSpeed = 100
 
-    if(this.isDeleting) {
-      typeSpeed /= 2;
+    if (this.isDeleting) {
+      typeSpeed /= 2
     }
 
-    if(!this.isDeleting && this.txt === fullTxt) {
-      typeSpeed = this.wait;
-      this.isDeleting = true;
-    } else if(this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.wordIndex++;
-      typeSpeed = 500;
+    if (!this.isDeleting && this.txt === fullTxt) {
+      typeSpeed = this.wait
+      this.isDeleting = true
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false
+      this.wordIndex++
+      typeSpeed = 500
     }
 
-    setTimeout(() => this.type(), typeSpeed);
+    setTimeout(() => this.type(), typeSpeed)
   }
 }
 
-
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init)
 
 function init() {
-  new Darkmode().showWidget();
-  const txtElement = document.querySelector('.txt-type');
-  const words = JSON.parse(txtElement.getAttribute('data-words'));
-  const wait = txtElement.getAttribute('data-wait');
-  new TypeWriter(txtElement, words, wait);
+  new Darkmode().showWidget()
+  const txtElement = document.querySelector('.txt-type')
+  const words = JSON.parse(txtElement.getAttribute('data-words'))
+  const wait = txtElement.getAttribute('data-wait')
+  new TypeWriter(txtElement, words, wait)
 }
 
 const targets = document.querySelectorAll('.animate')
 
 const lazyLoad = target => {
-  const io = new IntersectionObserver((entries, observer) => {
-    console.log(entries)
+  const io = new IntersectionObserver(
+    (entries, observer) => {
+      console.log(entries)
 
-    entries.forEach(entry => {
-      if(entry.isIntersecting) {
-        const img = entry.target
-        img.classList.add('fade')
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const img = entry.target
+          img.classList.add('fade')
 
-        observer.disconnect()
-      }
-    })
-  }, {
-    threshold: 1
-  })
+          observer.disconnect()
+        }
+      })
+    },
+    {
+      threshold: 1
+    }
+  )
   io.observe(target)
 }
 
 targets.forEach(lazyLoad)
-
